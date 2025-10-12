fb79ce4f39f920ebf4bbcef260863d1db3119843
Girthquake <captainpatchwork@gmail.com>
2025-10-11 21:05:34 -0800

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -11,6 +11,7 @@ class Tokenizer:
 			raise TypeError(f"Tokenizer data must be a string, provided type: '{type(data).__name__}'")
 		self._rw = remove_whitespace
 		self._rd = remove_duplicates
+		self._ct = {}
 
 	def __iter__(self):
 		cur_token = ""
@@ -32,7 +33,6 @@ class Tokenizer:
 					return token.strip()
 				return False
 			return token
-				
 
 		def yield_token(token=None):
 			nonlocal cur_token
@@ -41,6 +41,7 @@ class Tokenizer:
 				if check_remove_dup(token_to_yield):
 					if valid_token := check_remove_ws(token_to_yield):
 						yield valid_token
+				self._ct.setdefault(token_to_yield, self._ct.get(token_to_yield, 0) + 1)
 				cur_token = ""
 
 		def flush_yield(token):
@@ -49,34 +50,44 @@ class Tokenizer:
 			yield from yield_token(token)
 
 		i = -1
-		while i < len(data):
-			i += 1
-			char = data[i]
-			if char in Tokenizer.GROUPING_CHARS:
-				cur_token += char
-				continue
-
-			if char in Tokenizer.COMPOUND_CHARS:
-				if char == ".":
-					if data[i:i+3] == "...":
-						yield from flush_yield("...")
-						i = i + 2
-						continue
-
-				if char == ":":
-					if (next_colon := data.find(":", i + 1)) > 0:
-						possible_emoji = data[i:next_colon + 1]
-						if not any(c in string.whitespace for c in possible_emoji):
-							yield from flush_yield(possible_emoji)
-							i = next_colon + 1
-							continue
-
-				if data[i - 1] in Tokenizer.GROUPING_CHARS and data[i + 1] in Tokenizer.GROUPING_CHARS:
+		while True:
+			try:
+				i += 1
+				char = data[i]
+				if char in Tokenizer.GROUPING_CHARS:
 					cur_token += char
 					continue
 
-			else:
+				if char in Tokenizer.COMPOUND_CHARS:
+					if char == ".":
+						if data[i:i+3] == "...":
+							yield from flush_yield("...")
+							i = i + 2
+							continue
+
+					if char == ":":
+						if (next_colon := data.find(":", i + 1)) > 0:
+							possible_emoji = data[i:next_colon + 1]
+							if not any(c in string.whitespace for c in possible_emoji):
+								yield from flush_yield(possible_emoji)
+								i = next_colon + 1
+								continue
+						if cur_token.strip().casefold() == "https":
+							cur_token += char
+							continue
+
+						yield from flush_yield(":")
+						continue
+
+					if data[i - 1] in Tokenizer.GROUPING_CHARS and data[i + 1] in Tokenizer.GROUPING_CHARS:
+						cur_token += char
+						continue
+
 				yield from flush_yield(char)
+
+			except IndexError as e:
+				if "index out of range" in str(e):
+					pass
 
 		if cur_token:
 			yield from yield_token()
