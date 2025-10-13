5b472b01965f1680a345e1fd192df77328f65382
Girthquake <captainpatchwork@gmail.com>
2025-10-13 02:32:46 -0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -11,25 +11,3 @@ def indexalpha(data: str) -> int:
 		return ord(data) - ord("A" if data.isupper() else "a")
 	else:
 		return -1
-
-
-
-#def tokenize(data: str, remove_whitespace: bool = False, remove_punctuation: bool = False, remove_digits: bool = False, continuity_charset: Container[str] = ("'", *string.ascii_letters)) -> Iterator[str]:
-#	cur_token = ''
-#	for char in str(data):
-#		if char in continuity_charset:
-#			cur_token += char
-#		else:
-#			if cur_token:
-#				yield cur_token
-#				cur_token = ''
-#			if remove_whitespace and (char in string.whitespace):
-#				continue
-#			if remove_punctuation and (char in string.punctuation):
-#				continue
-#			if remove_digits and (char in string.digits):
-#				continue
-#
-#			yield char
-#	if cur_token:
-#		yield cur_token
