3620fe594f934b8b2ffff0dc3bb8aeb31cdf4033
Girthquake <captainpatchwork@gmail.com>
2025-10-17 23:40:59 -0800

    Update arithmetic.py

--- arithmetic.py
+++ arithmetic.py
@@ -14,6 +14,9 @@ def mode(*args: Any):
 			m = (c, a)
 	return m[1]
 
+def percentof(part, whole):
+	return (part / whole) * 100
+
 def roundup(arg: float):
 	return arg + (1 - (arg % 1))
 
@@ -36,3 +39,18 @@ def normalize_to_range(value, value_range: Sequence, new_range: Sequence):
 	vr = sorted(value_range)
 	return nr[len(vr) * ((value - vr[0]) // (vr[-1] - vr[0]))]
 
+if __name__ == "__main__":
+	"""
+	Starting value of 1, each iteration adds 1
+	logs, in percentage, how much each iteraction the value
+	has increased by overall with that iteration
+	"""
+	end = input("stop val  ")
+
+	log = []
+	a, b = 1, 2
+	while b <= int(end) + 1:
+		log.append(percentof(b, a) - 100)
+		a, b = b, b + 1
+	for idx, val in enumerate(log, start=1):
+		print(f"{idx}".ljust(5) + f"+{val:.2f}%".rjust(5))
